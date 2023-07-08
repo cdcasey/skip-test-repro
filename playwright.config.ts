2ffde2634a837a84551a260fd803e155551ceb52
Chris Casey <cdcasey@users.noreply.github.com>
2023-07-07 21:20:47 -0500

    don't need that

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,6 @@
-import { PlaywrightTestConfig, defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
-const baseURL = 'http://localhost:3000/';
-// const baseURL = process.env.NX_APP_ORIGIN || 'http://localhost:3000/'
-console.log({ baseURL });
-
-export const baseConfig: PlaywrightTestConfig = {
-  retries: 3,
-  maxFailures: 2,
-  workers: 1,
-  timeout: 45000,
-  use: {
-    baseURL,
-  },
-  reporter: [
-    [process.env.CI ? 'github' : 'list'],
-    // ['junit', { outputFile: '../../dist/playwright/reports/test-results.xml' }],
-    // ['json', { outputFile: '../../dist/playwright/reports/test-results.json' }],
-  ],
-};
+const baseURL = process.env.NX_APP_ORIGIN || 'http://localhost:3000/';
 
 export default defineConfig({
   retries: 3,
@@ -27,9 +10,4 @@ export default defineConfig({
   use: {
     baseURL,
   },
-  reporter: [
-    [process.env.CI ? 'github' : 'list'],
-    // ['junit', { outputFile: '../../dist/playwright/reports/test-results.xml' }],
-    // ['json', { outputFile: '../../dist/playwright/reports/test-results.json' }],
-  ],
 });
